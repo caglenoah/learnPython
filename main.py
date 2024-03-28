091cfce704f529d37c061d3c68858e24931b6719
caglenoah <caglenoah@gmail.com>
2024-03-28 00:07:28 -0600

    back to fundamentals :/ going to do it for java script too even though i already completed a 5 month boot camp lol. Be deligent, be patient. If you are actually confident enough to skip fundamentals you wouldnt be striving for more Knowledge

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 sword_damage = 10 
 player_health = 100
-health_after_attack = player_health + sword_damage
+health_after_attack = player_health - sword_damage
 
 print(f"Bad_Mamajama's health is: {player_health}")
 print(f"Bad_Mamajama is hit by a sword for {sword_damage} damage")
 print(f"Bad_Mamajama's health is now: {health_after_attack}")
+
+#DIRECTIONS
+#FIX THE MATH ERROR 

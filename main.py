bde250bbca45781865f2362ae85c19753a1a7877
caglenoah <caglenoah@gmail.com>
2024-03-28 15:04:36 -0600

    oops this is the actual new challenge

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-def hours_to_seconds(hours):
-    
+def become_warrior(first_name, last_name, power):
+    # ?
 
 
-# Don't touch below this line
+# Don't edit below this line
 
 
-def test(hours):
-    secs = hours_to_seconds(hours)
-    print(hours, "hours is", secs, "seconds")
+def main():
+    test("Frodo", "Baggins", 5)
+    test("Bilbo", "Baggins", 10)
+    test("Gandalf", "The Grey", 9000)
 
 
-test(10)
-test(1)
-test(25)
-test(100)
-test(33)
+def test(first_name, last_name, power):
+    title_string, power = become_warrior(first_name, last_name, power)
+    print(title_string, "has a power level of:", power)
+
+
+main()
+
+
+# DIRECTIONS
+# Complete the become_warrior function. It accepts 3 inputs:

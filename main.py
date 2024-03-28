b8504cf1564333957b5b5e44d744e6e2e5e1ffbe
caglenoah <caglenoah@gmail.com>
2024-03-28 00:54:35 -0600

    hopefully it is starting to pick up the pace

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 player_health = 1000
 
-# reduce by 100 here
+player_health = player_health - 100
 
 print(player_health)
 
-# and here
+player_health = player_health - 100
 
 print(player_health)
 
-# and here
+player_health = player_health - 100
 
 print(player_health)
 
-# and here
+player_health = player_health - 100
 
 print(player_health)
 

ad275d641c98e107251d4ae74a4f37feeb93935a
caglenoah <caglenoah@gmail.com>
2024-03-28 17:09:20 -0600

    these are getting funner, i was being dumb and trying to concatenate the return instead of just using a comma...

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def enchant_and_attack(player_health, damage, weapon):
-    # ?
+    enchanted_dam = damage + 10
+    new_health = player_health - enchanted_dam
+    enchanted_weapon = "enchanted" + weapon 
+    return enchanted_weapon, new_health
+    
 
 
 # Don't modify below this line
@@ -23,19 +27,3 @@ def main():
 main()
 
 
-#DIRECTIONS
-# Complete the enchant_and_attack function. It creates a new "enchanted" name for a weapon and calculates how much damage the enchanted weapon will deal to a victim.
-
-# It accepts 3 parameters:
-
-# player_health: integer
-# damage: integer
-# weapon: string
-# It should do the following things in the function body:
-
-# Calculate and store the "enchanted damage" in a new variable. The enchanted damage should be the input damage plus 10.
-# Calculate and store the "new health" in a new variable. The new health should be the input player_health minus the enchanted damage.
-# Create a new variable called enchanted_weapon. It should be the input weapon with the string "enchanted " added to the beginning of it. For example:
-# sword -> enchanted sword
-# axe -> enchanted axe
-# Return the enchanted weapon and the new health in that order.

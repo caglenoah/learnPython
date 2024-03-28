8f4b7354751f07a6361e7949ee7328760c9b7384
caglenoah <caglenoah@gmail.com>
2024-03-28 16:29:23 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,54 +1,21 @@
-def get_punched(health, armor=0):
-    health = int(health)
-    punched = -50
-    remaining_dam = armor + punched
-    remaining_health = health + remaining_dam
-    return remaining_health
-    
-
-
-
-
-
-
-def get_slashed(health, armor=0):
-    health = int(health)
-    slashed = -100
-    remaining_dam = armor + slashed
-    remaining_health = health + remaining_dam
-    return remaining_health
+def get_title(first_name, last_name, job):
+    title = first_name + " " + last_name + " the " + job
+    print(title)
 
 
 # Don't touch below this line
 
 
-def test(health, armor):
-    print(f"Health: {health}, Armor: {armor}")
-    print(f"Health after punch: {get_punched(health, armor)}")
-    print("=====================================")
-    print(f"Health: {health}, Armor: {armor}")
-    print(f"Health after slash: {get_slashed(health, armor)}\n")
-    print("=====================================")
-    print(f"Health: {health}, Armor: no armor!")
-    print(f"Health after slash: {get_slashed(health)}\n")
-    print("=====================================")
-    print(f"Health: {health}, Armor: no armor!")
-    print(f"Health after punch: {get_punched(health)}")
+def test(first_name, last_name, job):
+    title = get_title(first_name, last_name, job)
+    print("First name:", first_name)
+    print("Last name:", last_name)
+    print("Job:", job)
+    print("Title:", title)
     print("=====================================")
 
 
-test(400, 5)
-test(300, 3)
-test(200, 1)
-
-# Complete both the get_punched and get_slashed functions. They should each take 2 arguments:
-
-# health: An integer
-# armor: An integer
-# Change the functions so if no armor argument is passed, armor defaults to 0.
-
-# Each attack does a different amount of damage. Getting 
-# punched does 50 damage. Getting slashed does 100 damage. The armor should
-# absorb some of the attack by subtracting from the damage.
-# The rest of the damage should be applied to the health. Return a single integer, 
-# the health left over after the attack.
+test("Frodo", "Baggins", "warrior")
+test("Bilbo", "Baggins", "thief")
+test("Gandalf", "The Grey", "wizard")
+test("Aragorn", "Son of Arathorn", "ranger")

ffec77141e3a30a8d5abcca29aaa87675f14aced
caglenoah <caglenoah@gmail.com>
2024-03-28 14:51:11 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-def to_celsius(f):
-    return 5/9 * (f-32)
+def hours_to_seconds(hours):
+    
 
 
-## Don't touch below this line
+# Don't touch below this line
 
 
-def test(f):
-    c = round(to_celsius(f), 2)
-    print(f, "degrees fahrenheit is", c, "degrees celsius")
+def test(hours):
+    secs = hours_to_seconds(hours)
+    print(hours, "hours is", secs, "seconds")
 
 
+test(10)
+test(1)
+test(25)
 test(100)
-test(88)
-test(104)
-test(112)
+test(33)

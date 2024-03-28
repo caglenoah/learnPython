104abc950687cea828262ede0925b5e9f55d5b28
caglenoah <caglenoah@gmail.com>
2024-03-28 00:24:45 -0600

    reminds me of every time I needed help in the middle of class during the bootcamp. Fuckin typos lol

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Starting up server...')
-prnt("local server is listening on port 8080")
+print("Starting up server...")
+print("local server is listening on port 8080")
 
 # Fix the 2 errors in the code and get it to run!

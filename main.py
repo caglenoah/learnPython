570b0df7e6cc9282d08507da617918ed858da743
caglenoah <caglenoah@gmail.com>
2024-03-27 23:59:18 -0600

    firs python challenge..

---
+++ main.py
@@ -0,0 +1,7 @@
+sword_damage = 10 
+player_health = 100
+health_after_attack = player_health + sword_damage
+
+print(f"Bad_Mamajama's health is: {player_health}")
+print(f"Bad_Mamajama is hit by a sword for {sword_damage} damage")
+print(f"Bad_Mamajama's health is now: {health_after_attack}")

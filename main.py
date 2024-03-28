bf2b639ff27d45cc12f1b0d228952d899f49767d
caglenoah <caglenoah@gmail.com>
2024-03-28 00:49:22 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-items_in_cart = 5
-cost_of_each_item = 2.50
-total_cost = items_in_cart * cost_of_each_item
 
 
 
-print("Thank you for shopping at Bootmart!")
-print("Your total today was:") 
+print(player_health)
 
+# We need to keep track of our hero's health!
 
-
-print(f"${total_cost:.2f}")
-
-
-
-
-
-# This section of the report should print:
-
-# Thank you for shopping at Bootmart!
-# Your total today was:
-# Fix the bug in the print statements.
+# On the first line of code, create a new variable named player_health and set it equal to 1000.

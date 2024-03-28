c2c3cc314ed1c6b2818abf90880fcf8ed5cbaae0
caglenoah <caglenoah@gmail.com>
2024-03-28 00:44:40 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-print((250 + 241 + 244 + 255) / 4)
+items_in_cart = 5
+cost_of_each_item = 2.50
+total_cost = items_in_cart * cost_of_each_item
+
+# Don't Touch Above This line
+
+print "Thank you for shopping at Bootmart!"
+print ""
+
+# Don't Touch Below This Line
+
+print(f"${total_cost:.2f}")
 
 
-# Debug the code so that it correctly prints the solution to the math problem.
+
+
+
+# This section of the report should print:
+
+# Thank you for shopping at Bootmart!
+# Your total today was:
+# Copy icon
+# Fix the bug in the print statements.

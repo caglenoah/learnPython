a259a94ab4ed26422bb24bdb590b7ed13947896f
caglenoah <caglenoah@gmail.com>
2024-03-28 00:16:20 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-print(250 + 75)
+print("Hero: ...")
+print("Jax: Where are you off to this morning? Bkaw...")
+print("Hero: Where did an owl learn to speak??")
+print("Jax: B-Kaw!")
 
-# DIRECTIONS
-#Our hero's sword does 250 normally but does an additional 75 damage when it's enchanted.
-#Calculate and print the result of 250 + 75.
 
+
+
+# Rearrange the code so that it prints in the following order.
+
+# Jax: B-Kaw!
+# Hero: ...
+# Jax: Where are you off to this morning? Bkaw...
+# Hero: Where did an owl learn to speak??

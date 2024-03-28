5b54988005ad62fb23224b58c63d797651df6336
caglenoah <caglenoah@gmail.com>
2024-03-28 14:42:10 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-dmg_one = 2
-dmg_two = 4
-dmg_three = 3
-dmg_four = -1
-dmg_five = 10
-dmg_six = 5
-
-# Don't touch above this line
+def to_celsius(f):
+    # ?
 
 
-def calculate_damage(opening_attack, core_damage, finishing_move):
-    return opening_attack + core_damage + finishing_move
+## Don't touch below this line
 
 
-# Don't touch below this line
+def test(f):
+    c = round(to_celsius(f), 2)
+    print(f, "degrees fahrenheit is", c, "degrees celsius")
 
-print("Getting damage for", dmg_one, dmg_two, "and", dmg_three, "...")
-print(calculate_damage(dmg_one, dmg_two, dmg_three), "points of damage dealt!")
-print("=====================================")
 
-print("Getting damage for", dmg_four, dmg_five, "and", dmg_six, "...")
-print(calculate_damage(dmg_four, dmg_five, dmg_six), "points of damage dealt!")
-print("=====================================")
+test(100)
+test(88)
+test(104)
+test(112)

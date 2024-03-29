43fbb47a543b739bb629756b7eed28d99ae67943
caglenoah <caglenoah@gmail.com>
2024-03-28 23:31:30 -0600

    had a typo on line 4, didnt have a space between - and actual_damage, hard to code with crazy nephews around lol

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def take_magic_damage(health, resist, amp, spell_power):
-   max_damage = amp * spell_power
-   actual_damage = max_damage - resist
-   new_health = health - actual_damage
-   return new_health
+    max_damage = amp * spell_power
+    actual_damage = max_damage - resist
+    new_health = health - actual_damage
+    return new_health

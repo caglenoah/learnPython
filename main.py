5592dd66f780d403b028f4edce7c6dd08a6152e2
caglenoah <caglenoah@gmail.com>
2024-03-30 18:12:47 -0600

    gnu challenge

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def create_stats_message(strength, wisdom, dexterity):
-    total = strength + wisdom + dexterity
-    msg = f"You have {strength} strength, {wisdom} wisdom, and {dexterity} dexterity for a total of {total} stats."
-    return msg
+def calculate_damage(sword, arrow, spear, dagger, fire):
+    total_damage = 0
+    average_damage = 0
+    return total_damage, average_damage
--- main_test.py
+++ main_test.py
@@ -1,27 +1,25 @@
 from main import *
 
 run_cases = [
-    (1, 2, 3, "You have 1 strength, 2 wisdom, and 3 dexterity for a total of 6 stats."),
-    (2, 4, 2, "You have 2 strength, 4 wisdom, and 2 dexterity for a total of 8 stats."),
+    (3, 5, 2, 1, 4, (15, 3.0)),
+    (5, 5, 5, 5, 5, (25, 5.0)),
 ]
 
 submit_cases = run_cases + [
-    (
-        10,
-        50,
-        100,
-        "You have 10 strength, 50 wisdom, and 100 dexterity for a total of 160 stats.",
-    ),
-    (0, 0, 0, "You have 0 strength, 0 wisdom, and 0 dexterity for a total of 0 stats."),
-    (1, 1, 1, "You have 1 strength, 1 wisdom, and 1 dexterity for a total of 3 stats."),
+    (1, 2, 3, 4, 5, (15, 3.0)),
+    (0, 0, 0, 0, 10, (10, 2.0)),
+    (0, 0, 0, 0, 0, (0, 0.0)),
+    (10, 20, 30, 40, 50, (150, 30.0)),
+    (2, 2, 2, 2, 2, (10, 2.0)),
+    (1, 1, 1, 1, 1, (5, 1.0)),
 ]
 
 
-def test(input1, input2, input3, expected_output):
+def test(sword, arrow, spear, dagger, fire, expected_output):
     print("---------------------------------")
-    print(f"Inputs: {input1}, {input2}, {input3}")
+    print(f"Inputs: {sword}, {arrow}, {spear}, {dagger}, {fire}")
     print(f"Expecting: {expected_output}")
-    result = create_stats_message(input1, input2, input3)
+    result = calculate_damage(sword, arrow, spear, dagger, fire)
     print(f"Actual: {result}")
     if result == expected_output:
         print("Pass")

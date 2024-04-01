8762585167a0261f79733430cda76198561128b9
caglenoah <caglenoah@gmail.com>
2024-04-01 16:10:14 -0600

    maths

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def calculate_squares(start, end):
+ for i in range(start, end, 1):
+        num = i
+        num_squared = i * i
+        print(num, " squared = ", num_squared )
 
+        
 def test(start, end):
     print(f"Calculating squares from {start} to {end - 1}:")
     calculate_squares(start, end)

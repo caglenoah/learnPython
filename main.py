a1468ffd41665caa1bae0f095bb9ececfe9ed4c8
caglenoah <caglenoah@gmail.com>
2024-04-01 05:01:08 -0600

    if else elif, elif is neeeeeeatooo

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def combat_evaluation(player_power, enemy_defense):
     advantage, disadvantage, evenly_matched = False, False, False
 
+    if  player_power > enemy_defense:
+        advantage = True
+    elif player_power == enemy_defense:
+        evenly_matched = True
+    else:
+        disadvantage = True
 
-     return advantage, disadvantage, evenly_matched
+    return advantage, disadvantage, evenly_matched

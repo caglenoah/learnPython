ef0a05032222d785e209b2a9af9106cb0284ecb3
caglenoah <caglenoah@gmail.com>
2024-04-01 15:58:44 -0600

    for loop from scratch

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def countdown_to_blastoff():
+    for i in range(10, 0, -1):
+        if i > 1:
+            print(i, "...")
+        else:
+            print (i, "...Blastoff!")
 
 
 
-
-    
 def test():
     print("Counting down to blastoff:")
     countdown_to_blastoff()

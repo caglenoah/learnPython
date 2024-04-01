46fc4f6654d903d6833663bf9c21a18f2102d1f6
caglenoah <caglenoah@gmail.com>
2024-04-01 15:56:40 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def sum_of_odd_numbers(end):
-    total = 0
-    for i in range(1, end, 2):
-        total += i
-    return total
+def countdown_to_blastoff():
+
+
+
+
+    
+def test():
+    print("Counting down to blastoff:")
+    countdown_to_blastoff()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()

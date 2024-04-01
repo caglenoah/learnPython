3b830ac32d50358533eaa8ea5d0add7888c84911
caglenoah <caglenoah@gmail.com>
2024-04-01 04:33:05 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-def player_status(health):
-    if health <= 0:
-        return "dead"
-    elif health <= 5:
-        return "injured"
-    else:
-        return "healthy"
+def does_attack_hit(attack_roll, armor_class):

a408da5bdf2a28a00a95b4f6183c911a6f5cc011
caglenoah <caglenoah@gmail.com>
2024-04-01 05:28:43 -0600

    to set the iteration there is a 3rd optional perameter. Here i set it to -1 to countdown from start to finish. Didnt include the whole challenge. More of a note for myself

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-def print_numbers():
-    for i in range (0, 100):
+def count_down(start, end):
+    for i in range(start, end, -1):
         print(i)
-
-
-# Don't edit below this line
-
-
-def test():
-    print("Printing numbers from 0 to 99:")
-    print_numbers()
-    print("=====================================")
-
-
-def main():
-    test()

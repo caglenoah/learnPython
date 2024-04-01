f688dd9b0aa4b78863ec8993bc939dc8467bfdd5
caglenoah <caglenoah@gmail.com>
2024-03-31 20:27:37 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-def get_hurt(current_health, damage):
-    current_health = current_health - damage
-    return current_health
+can_create_guild = 0b1000
+can_review_guild = 0b0100
+can_delete_guild = 0b0010
+can_edit_guild = 0b0001
+
+
+def can_create_bits(user_permissions):
+    return user
+
+
+def can_review_bits(user_permissions):
+    pass
+
+
+def can_delete_bits(user_permissions):
+    pass
+
+
+def can_edit_bits(user_permissions):
+    pass

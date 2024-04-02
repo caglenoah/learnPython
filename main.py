e24ae0dce74c6ede9ebea005792a7bd4dd68f5c2
caglenoah <caglenoah@gmail.com>
2024-04-01 21:01:47 -0600

    logic breakdown

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 def is_prime(number):
+
+
+
+     #write a for loop that starts at 2 and ends at number
+    # have number % by i if there is a remainder move on to the next i
+    #if there is no remainder stop loop and return false
+    #if loop completes return true

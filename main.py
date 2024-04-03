125c3c0d534dc4800068b3c5a57b7b70859b3b77
caglenoah <caglenoah@gmail.com>
2024-04-03 02:21:43 -0600

    i was pretty stoke on my code but wanted to see if there is also an easier way to compare two lists. Discovered the zip method. Pretty neat. In order for either way of writing this code to work the lists must be the same length

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 def get_test_score(answer_sheet, student_answers):
-    correct_ans = 0 
-    for i in range(len(answer_sheet)):
-        if answer_sheet[i] == student_answers[i]:
+    correct_ans = 0
+    for ans_key, ans_student in zip(answer_sheet, student_answers):
+        if ans_key == ans_student:
             correct_ans += 1
     percentage = (correct_ans / len(answer_sheet)) * 100
     return percentage
+        
+
+
+
+
+
+
+
+
+
+#my original code
+# def get_test_score(answer_sheet, student_answers):
+#     correct_ans = 0 
+#     for i in range(len(answer_sheet)):
+#         if answer_sheet[i] == student_answers[i]:
+#             correct_ans += 1
+#     percentage = (correct_ans / len(answer_sheet)) * 100
+#     return percentage

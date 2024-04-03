f566260c0aa7d280623654a613a7b88157d90932
caglenoah <caglenoah@gmail.com>
2024-04-02 21:49:37 -0600

    deleting be simple to dogger

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 def trim_strongholds(strongholds):
+    del strongholds[0] 
+    del strongholds[-2:]
+    return strongholds
+
+
+
+
+# Delete the first stronghold from the list
+# Delete the last two strongholds from the list
+# Return the new trimmed-down list

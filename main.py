b6a123b2160e1336c82b0f45c4bbc76227ca1b77
caglenoah <caglenoah@gmail.com>
2024-04-02 19:53:47 -0600

    added both ways to write the function. Also I almost had it first try, but i let white space get the better of me again. wrote the function correctly but had the return statement indended on accident.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 def find_max(nums):
     max_so_far = float("-inf")
+    for num in nums:
+        if num > max_so_far:
+            max_so_far = num
+    return max_so_far
+
+# def find_max(nums):
+#     max_so_far = float("-inf")
+#     for i in range(0, len(nums)):
+#         if nums[i] > max_so_far:
+#             max_so_far = nums[i]
+#     return max_so_far

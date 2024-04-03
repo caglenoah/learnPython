b145c2d991c1ecafa2e2ce2df019d7004eb9aa15
caglenoah <caglenoah@gmail.com>
2024-04-02 22:36:49 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-def get_first_item(items):
-    if items == []:
-        return "ERROR"
-    return items[0]
-
-
-   
-   
-   
-    # . Write a function that returns the first element from a list.
-    # If the list is empty then return the string ERROR instead.
+def reverse_array(items):

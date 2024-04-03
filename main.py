7538fb87460ef6630533ca05427d0c06f68429fe
caglenoah <caglenoah@gmail.com>
2024-04-02 19:27:20 -0600

    new challenge. The reason i only have to specify one of the lists in the loop is because the list i am comparing it to is the same length

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-def contains_leather_scraps(items):
-    found = False
+def check_character_levels():
+    old_character_levels = [1, 42, 43, 53, 12, 3, 32, 34, 54, 32, 43]
+    new_character_levels = [1, 42, 45, 54, 12, 3, 32, 38, 54, 32, 42]
 
     # don't touch above this line
 
-    for item in items:
-        if item == "Leather Scraps":
-            found = True
+    for i in range(0, len(old_character_levels)):
+        # ?
 
-    # don't touch below this line
 
-    return found
+# don't touch below this line
+
+
+def test():
+    print("Character level increased at indexes:")
+    check_character_levels()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()

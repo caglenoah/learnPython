6dbbd280243aa00bed33546859315a526380f516
caglenoah <caglenoah@gmail.com>
2024-04-03 01:24:17 -0600

    getting better with white space

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 def get_odds_and_evens(numbers):
+    num_evens = 0
+    num_odds = 0
+    for number in numbers:
+        if number % 2 == 0:
+            num_evens += 1
+        else:
+            num_odds += 1
+    return num_odds, num_evens
+
 
 
 

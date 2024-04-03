91f48e458dca30daf6dfa5a2234aea6bcca2536b
caglenoah <caglenoah@gmail.com>
2024-04-02 20:06:41 -0600

    now i had an issue opposite of what i did a few times ago. The first time i just used i, this time before i commited i was using num[i]. Lol i understand why you do it ifferent in each case now. Pay attention.

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def get_odd_numbers(num):
     odd_numbers = []
 
     for i in range(0, num):
-        # don't touch above this line
-        pass
+        if i % 2 != 0:
+            odd_numbers.append(i)
+
 
     # don't touch below this line
 

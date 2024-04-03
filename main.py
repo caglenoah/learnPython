48b8b6ec0f3437221e84b076e5da7c69071e0224
caglenoah <caglenoah@gmail.com>
2024-04-03 02:13:36 -0600

    okay pretty stoke on that one actually, even got the white space correct first try

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 def get_test_score(answer_sheet, student_answers):
+    correct_ans = 0 
+    for i in range(len(answer_sheet)):
+        if answer_sheet[i] == student_answers[i]:
+            correct_ans += 1
+    percentage = (correct_ans / len(answer_sheet)) * 100
+    return percentage

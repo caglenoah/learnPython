7072044e0fb5815a00bf53daa9c022d0ea5badc4
caglenoah <caglenoah@gmail.com>
2024-04-02 21:58:02 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-def trim_strongholds(strongholds):
-    del strongholds[0] 
-    del strongholds[-2:]
-    return strongholds
+def get_heroes():
+    heroes = [
+        "Glorfindel",
+        2093,
+        True,
+        "Gandalf",
+        1054,
+        False,
+        "Gimli",
+        389,
+        False,
+        "Aragorn",
+        87,
+        False,
+    ]
+
+    return heroes
+
+
+# Don't touch below this line
+
+
+def test():
+    heroes = get_heroes()
+    for hero in heroes:
+        print(f"name: {hero[0]}, age: {hero[1]}, is_elf: {hero[2]}")
+
+
+def main():
+    test()
+
+
+main()
 
 
 
 
-# Delete the first stronghold from the list
-# Delete the last two strongholds from the list
-# Return the new trimmed-down list
+

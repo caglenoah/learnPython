ff1d944f01e2a23da25601035fc7c1aac0d70fb0
caglenoah <caglenoah@gmail.com>
2024-04-04 15:29:17 -0600

    unexpected vowel movement... I'm hilarious

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 def count_vowels(text):
+    vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+    vowels_count = 0
+    unique_vowels = set()
+    for char in text:
+        if char in vowels:
+            vowels_count += 1
+            unique_vowels.add(char)
+    return vowels_count, unique_vowels

45f3b5649221d8c03da3caf8f890acb79064a544
caglenoah <caglenoah@gmail.com>
2024-04-04 00:35:15 -0600

    i am tired an had to use external resources and found item(). Came back and wrote it the way the course was teaching me using []. Both work. Sleepy time.

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 def get_most_common_enemy(enemies_dict):
+    max_count = float("-inf")
+    most_common_enemy = None
+    for enemy, count in enemies_dict.items():
+        if count > max_count:
+            max_count = count
+            most_common_enemy = enemy
+    return most_common_enemy
+
+
+
+
+
+
+
+
+
+# def get_most_common_enemy(enemies_dict):
+#     max_count = float("-inf")
+#     most_common_enemy = None
+#     for enemy_name in enemies_dict:
+#         count = enemies_dict[enemy_name]
+#         if count > max_count:
+#             max_count = count
+#             most_common_enemy = enemy_name
+#     return most_common_enemy

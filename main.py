afaccd453e9d2559de5ebdb8a2c46c8c3f341d9b
caglenoah <caglenoah@gmail.com>
2024-04-04 16:07:31 -0600

    had this way to complicated the first time. Had two different for loops and an if comparison. Kiss. dont forget to kiss the code

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 def find_missing_ids(first_ids, second_ids):
+    id_set1 = set(first_ids)
+    id_set2 = set(second_ids)
+    missing_ids_set = id_set1 - id_set2
+    missing_ids = list(missing_ids_set)
+    return missing_ids

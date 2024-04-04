5f6abbbbe78dcb0528f2f67ff50b668af6159744
caglenoah <caglenoah@gmail.com>
2024-04-03 19:50:41 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-def get_test_score(answer_sheet, student_answers):
-        name = student_answers.pop(0)
-        correct_count = 0
-        for i in range(0, len(answer_sheet)):
-            if answer_sheet[i] == student_answers[i]:
-                correct_count += 1
-        percentage = (correct_count / len(answer_sheet)) * 100
-        return name, percentage
+def double_string(string):

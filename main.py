b745ecf12192cf07e3a7b00297f4394ecad64d17
caglenoah <caglenoah@gmail.com>
2024-04-04 14:29:17 -0600

    merging dictionaries and returning total values. Fegure out what the code was actually doing, then wrote it in cleaner ways

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 def merge(dict1, dict2):
-    merged_scores = {
-        "first_quarter": dict1["first_quarter"] if "first_quarter" in dict1 else 0,
-        "second_quarter": dict1["second_quarter"] if "second_quarter" in dict1 else 0,
-        "third_quarter": dict2["third_quarter"] if "third_quarter" in dict2 else 0,
-        "fourth_quarter": dict2["fourth_quarter"] if "fourth_quarter" in dict2 else 0,
-    }
-    return merged_scores
-
+    merged_dict = {}
+    for k in dict1:
+        merged_dict[k] = dict1[k]
+    for k in dict_2:
+        merged_dict[k] = dict2[k]
+    return merged_dict
    
 
 def total_score(score_dict):
@@ -16,6 +14,14 @@ def total_score(score_dict):
     return total
     
 
+# def merge(dict1, dict2):
+#     merged_scores = {
+#         "first_quarter": dict1["first_quarter"] if "first_quarter" in dict1 else 0,
+#         "second_quarter": dict1["second_quarter"] if "second_quarter" in dict1 else 0,
+#         "third_quarter": dict2["third_quarter"] if "third_quarter" in dict2 else 0,
+#         "fourth_quarter": dict2["fourth_quarter"] if "fourth_quarter" in dict2 else 0,
+#     }
+#     return merged_scores
 # def total_score(score_dict):
 #     total = 0
 #     total += score_dict["first_quarter"]

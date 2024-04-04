f59e93f9777b4372948409edc154c5d6c3f2c9b4
caglenoah <caglenoah@gmail.com>
2024-04-03 21:07:45 -0600

    dang had to use resources for that one. Makes sense at least but the f string through me off a bit. And it took me way to long to realize i needed a second for loop.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-def get_character_record(name, server, level, rank):
-    get_character_record = {
-        "name": name,
-        "server": server,
-        "level": level,
-        "rank": rank,
-        "id": f"{name}#{server}"
-    }
-    return get_character_record
+def count_enemies(enemy_names):
+    enemies_dict = {}
+    for enemy_name in enemy_names:
+        enemy = enemy_name.split()
+        enemy_type_count = 0
+        for name in enemy_names:
+            if name == enemy_name:
+                enemy_type_count += 1
+        enemies_dict[f"{' '.join(enemy)}"] = enemy_type_count
+        return enemies_dict
+

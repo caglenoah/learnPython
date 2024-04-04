d09f63030b8cc138124e4649a7631687ca4e698a
caglenoah <caglenoah@gmail.com>
2024-04-04 16:04:30 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-def count_vowels(text):
-    vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
-    vowels_count = 0
-    unique_vowels = set()
-    for char in text:
-        if char in vowels:
-            vowels_count += 1
-            unique_vowels.add(char)
-    return vowels_count, unique_vowels
+def find_missing_ids(first_ids, second_ids):

ba9254cec352c0e786e8251a88b253d867ac1f60
caglenoah <caglenoah@gmail.com>
2024-04-04 14:49:52 -0600

    getting tired and a bit burnt out, brains not working right lol. Keep pushing through, you get to kayak and seee your friends this weekend woooop! barrel springs, shoshone, and friends i have neglected for my future, fuck ya

--- main.py
+++ main.py
@@ -12,4 +12,17 @@ def calculate_total(items_purchased, grocery_list):
         "cereal": 5.99,
     }
 
-   
+    unpurchased_items = []
+    receipt = {}
+
+    for item in items_purchased:
+        if item in item_prices:
+            receipt[item] = item_prices[item]
+        
+    for item in grocery_list:
+        if item not in items_purchased:
+            unpurchased_items.append(item)
+
+    total = sum(receipt.values())
+
+    return unpurchased_items, receipt, total

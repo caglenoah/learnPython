996d799091827561ccea283c511e033ee42fdfa0
caglenoah <caglenoah@gmail.com>
2024-04-05 23:00:33 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-def handle_get_player_record(player_id):
-    try:
-        return get_player_record(player_id)
-    except IndexError:
-        return "index is too high"
-    except Exception as e:
-        return e
+def make_purchases(purchase_orders):
+    # ?
+
 
 # Don't edit below this line
 
 
-def get_player_record(player_id):
-    if player_id < 0:
-        raise Exception("negative ids not allowed")
-    players = [
-        {"name": "Slayer", "level": 128},
-        {"name": "Dorgoth", "level": 300},
-        {"name": "Saruman", "level": 4000},
-    ]
-    return players[player_id]
+def main():
+    print("Making purchases...")
+    leftovers = make_purchases(
+        [
+            {"price": 10.00, "money_available": 125.00},
+            {"price": 5.00, "money_available": 2.00},
+            {"price": 20.01, "money_available": 5.20},
+            {"price": 1.04, "money_available": 254.00},
+            {"price": 40.20, "money_available": 6.00},
+            {"price": 16.00, "money_available": 235.01},
+            {"price": 10.70, "money_available": 10.70},
+            {"price": 12.00, "money_available": 2.30},
+        ]
+    )
+    print("Purchases complete!")
+    print("Leftover amounts for successful purchases:")
+    for leftover in leftovers:
+        print(f" * {leftover:.2f}")
+
+
+def purchase(price, money_available):
+    if money_available < price:
+        raise Exception(f"{money_available:.2f} is not enough for {price:.2f}")
+    return money_available - price
+
+
+main()

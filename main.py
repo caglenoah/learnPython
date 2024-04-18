c518d67dfb2f1915d3a610bccdcdf580e13de914
caglenoah <caglenoah@gmail.com>
2024-04-17 20:50:22 -0600

    fizzbuzz

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 def fizzbuzz(start, end):
+    for num in range(start, end):
+        if num % 3 == 0 and num % 5 == 0:
+            print("fizzbuzz")
+        elif num % 3 == 0:
+            print("fizz")
+        elif num % 5 == 0:
+            print("buzz")
+        else:
+            print(num)
+
+def main():
+    test(1, 100)
+    test(5, 30)
+    test(1, 15)
+
+
+def test(start, end):
+    print("Starting test")
+    fizzbuzz(start, end)
+    print("======================")
+
+
+main()

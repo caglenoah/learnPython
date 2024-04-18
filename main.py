793f034c33a8179f1544b452670b11b4d99a4b8b
caglenoah <caglenoah@gmail.com>
2024-04-17 20:48:32 -0600

    new challenge

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-def area_sum(rectangles):
-    total_area = 0
-    for rectangle in rectangles:
-        area = rectangle['height'] * rectangle ['width']
-        total_area += area
-    return total_area
+def fizzbuzz(start, end):

54260f8a97db1841087b7490a252ecb7de412aca
caglenoah <caglenoah@gmail.com>
2024-04-19 18:49:49 -0600

    found the enumerate method

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 def join_strings(strings):
+    con_strings = ""
+    for index, string in enumerate(strings):
+        con_strings += string
+        if index < len(strings) - 1:
+            con_strings += ","
+    return con_strings
+        
